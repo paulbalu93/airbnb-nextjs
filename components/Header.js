7b15c89ca06b52cb1a2a68368bc8fe4df41a5a9f
Paul Maliakel <paulbalu93@gmail.com>
2021-08-06 14:53:45 +0200

    adds search page

--- components/Header.js
+++ components/Header.js
@@ -10,10 +10,16 @@ import { useState } from "react";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css";
 import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
 function Header() {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
+  const router = useRouter();
+  const resetInput = () => {
+    setSearchInput("");
+  };
   const selectionRange = {
     startDate: startDate,
     endDate: endDate,
@@ -23,10 +29,25 @@ function Header() {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
   };
+
+  const search = () => {
+    router.push({
+      pathname: "search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md px-5 py-5 md:px-10">
       {/* left-logo */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto ">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto "
+      >
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -72,6 +93,21 @@ function Header() {
               Number of guests
             </h2>
             <UsersIcon className="h-5" />
+            <input
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              type="number"
+              className="w-12 pl-2 text-lg text-red-400"
+              min={1}
+            ></input>
+          </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button onClick={search} className="flex-grow text-red-400">
+              Search
+            </button>
           </div>
         </div>
       )}
